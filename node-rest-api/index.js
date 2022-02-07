febc9652b752df79d740de29a5114c2a20501951
Prakash Das <prakashdas18@gmail.com>
2022-02-07 15:39:46 +0530

    creating express app

---
+++ node-rest-api/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+app.listen(8800, () => {
+    console.log("Backend server is running");
+})
