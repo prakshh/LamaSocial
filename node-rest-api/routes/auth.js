066765da573322e0950900129e76be4bbb899a70
Prakash Das <prakashdas18@gmail.com>
2022-02-08 10:54:59 +0530

    post testing for register via Postman and MongoDB

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -5,15 +5,30 @@ const User = require("../models/User");
 //     res.send("hey, its auth route");
 // })
 
+// //REGISTER - testing
+// router.get("/register", async (req, res) => {
+//     const user = await new User({
+//         username: "wick",
+//         email: "wick@gmail.com",
+//         password: "7891011"
+//     })
+//     await user.save();
+//     res.send("ok");
+// })
+
 //REGISTER
-router.get("/register", async (req, res) => {
-    const user = await new User({
-        username: "wick",
-        email: "wick@gmail.com",
-        password: "7891011"
-    })
-    await user.save();
-    res.send("ok");
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    try{
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch(err){
+        console.log(err);
+    }
 })
 
 module.exports = router
