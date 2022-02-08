c76150dd60202ebb99e7d2091c61caf349477056
Prakash Das <prakashdas18@gmail.com>
2022-02-08 11:45:54 +0530

    login auth done

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -35,7 +35,24 @@ router.post("/register", async (req, res) => {
         const user = await newUser.save();
         res.status(200).json(user);
     } catch(err){
-        console.log(err);
+        // console.log(err);
+        res.status(500).json(err)
+    }
+});
+
+//LOGIN
+router.post("/login", async (req,res) => {
+    try{
+        const user = await User.findOne({ email: req.body.email });
+        !user && res.status(404).json("user not found");
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("wrong password")
+
+        res.status(200).json(user)
+    } catch (err) {
+        // console.log(err);
+        res.status(500).json(err)
     }
 })
 
