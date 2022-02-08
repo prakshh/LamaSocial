46600b156d9a258ac4d03b4cee164912c08226e1
Prakash Das <prakashdas18@gmail.com>
2022-02-08 10:27:50 +0530

    testing register

--- node-rest-api/routes/auth.js
+++ node-rest-api/routes/auth.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req,res) => {
-    res.send("hey, its auth route");
+// router.get("/", (req,res) => {
+//     res.send("hey, its auth route");
+// })
+
+//REGISTER
+router.get("/register", async (req, res) => {
+    const user = await new User({
+        username: "wick",
+        email: "wick@gmail.com",
+        password: "7891011"
+    })
+    await user.save();
+    res.send("ok");
 })
+
 module.exports = router
