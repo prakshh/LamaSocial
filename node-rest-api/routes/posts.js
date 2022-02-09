bd86cb6e137c6227f43eb23a415bd5e175751256
Prakash Das <prakashdas18@gmail.com>
2022-02-09 15:30:50 +0530

    require Model User

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 // router.get("/", (req, res) => {
 //     console.log("post page");
