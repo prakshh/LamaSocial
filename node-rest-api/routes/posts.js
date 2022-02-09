f01b62d1ef092c6254e378253df36557882100f7
Prakash Das <prakashdas18@gmail.com>
2022-02-09 14:59:04 +0530

    like and unlike posts

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -49,4 +49,21 @@ router.delete("/:id", async (req, res) => {
     }
   });
 
+//like / dislike a post
+
+router.put("/:id/like", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (!post.likes.includes(req.body.userId)) {
+        await post.updateOne({ $push: { likes: req.body.userId } });
+        res.status(200).json("The post has been liked");
+      } else {
+        await post.updateOne({ $pull: { likes: req.body.userId } });
+        res.status(200).json("The post has been disliked");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router; 
