2931d08ec5e6bf291f831b5f09e88bc21074b468
Prakash Das <prakashdas18@gmail.com>
2022-02-09 14:38:14 +0530

    post : create, update, delete

--- node-rest-api/routes/posts.js
+++ node-rest-api/routes/posts.js
@@ -1,7 +1,52 @@
 const router = require("express").Router();
+const Post = require("../models/Post");
 
-router.get("/", (req, res) => {
-    console.log("post page");
-})
+// router.get("/", (req, res) => {
+//     console.log("post page");
+// })
+
+//create a post
+
+router.post("/", async (req, res) => {
+    const newPost = new Post(req.body);
+    try {
+      const savedPost = await newPost.save();
+      res.status(200).json(savedPost);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//update a post
+
+router.put("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.updateOne({ $set: req.body });
+        res.status(200).json("the post has been updated");
+      } else {
+        res.status(403).json("you can update only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//delete a post
+
+router.delete("/:id", async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+      if (post.userId === req.body.userId) {
+        await post.deleteOne();
+        res.status(200).json("the post has been deleted");
+      } else {
+        res.status(403).json("you can delete only your post");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router; 
