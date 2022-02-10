69fb5816a375e14b87a61fdba3f2943f1fde357a
Prakash Das <prakashdas18@gmail.com>
2022-02-10 08:07:11 +0530

    topbar component

--- react-social/src/pages/home/Home.js
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-    return (
-        <div>this is homepage</div>
-    )
-}
---
+++ react-social/src/pages/home/Home.jsx
@@ -0,0 +1,7 @@
+import Topbar from "../../components/topbar/Topbar";
+
+export default function Home() {
+    return (
+        <div><Topbar /></div>
+    )
+}
