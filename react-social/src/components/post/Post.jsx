ee4befe42c73746bf344038173aa042b5208a661
Prakash Das <prakashdas18@gmail.com>
2022-02-12 09:19:55 +0530

    Like Functionality with useState Hook

--- react-social/src/components/post/Post.jsx
+++ react-social/src/components/post/Post.jsx
@@ -1,11 +1,23 @@
 import { ContactsOutlined, MoreVert } from "@material-ui/icons";
 import "./post.css";
 import { Users } from "../../dummyData";
+import { useState } from "react";
 
 export default function Post({post}) {
 //   console.log(post);
 // const user = Users.filter(u=>u.id===1)
 // console.log(user[0].username) 
+
+// Like Functionality with useState Hook
+
+const [like, setLike] = useState(post.like);
+const [isLiked, setIsLiked] = useState(false);
+
+const likeHandler = () => {
+    setLike(isLiked ? like-1 : like+1)
+    setIsLiked(!isLiked);
+}
+
   return (
     <div className="post">
         <div className="postWrapper">
@@ -29,9 +41,9 @@ export default function Post({post}) {
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img className="likeIcon" src="/assets/like.png" alt="" />
-                    <img className="likeIcon" src="/assets/heart.png" alt="" />
-                    <span className="postLikeCounter">{post.like} people like it</span>
+                    <img className="likeIcon" src="/assets/like.png" onClick={likeHandler} alt="" />
+                    <img className="likeIcon" src="/assets/heart.png" onClick={likeHandler} alt="" />
+                    <span className="postLikeCounter">{like} people like it</span>
                 </div>
                 <div className="postBottomRight">
                 <span className="postCommentText">{post.comment} comments</span>
