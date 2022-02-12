34a8c77f58a7b485d9612e532beebdb6aea9dde4
Prakash Das <prakashdas18@gmail.com>
2022-02-12 20:17:51 +0530

    profilePage - rightBar

--- react-social/src/pages/profile/Profile.jsx
+++ react-social/src/pages/profile/Profile.jsx
@@ -31,7 +31,7 @@ export default function Profile() {
             </div>
             <div className="profileRightBottom">
               <Feed />
-              <Rightbar />
+              <Rightbar profile />
             </div>
           </div>
         </div>  
