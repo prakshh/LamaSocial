79c9255b7a631f1feae72d2140abbb58f521d98c
Prakash Das <prakashdas18@gmail.com>
2022-02-25 13:30:10 +0530

    fetching data with Axios and Hooks

--- mern-social-media/client/src/components/feed/Feed.jsx
+++ mern-social-media/client/src/components/feed/Feed.jsx
@@ -1,14 +1,36 @@
-import { Posts } from "../../dummyData";
+// import { Posts } from "../../dummyData";
+import { useEffect, useState } from "react";
 import Post from "../post/Post";
 import Share from "../share/Share";
 import "./feed.css";
+import axios from "axios";
 
 export default function Feed() {
+  
+  /*
+  const [posts, setPosts] = useState([]);
+  const [text, setText] = useState("")
+  useEffect(() => {
+    console.log("feed rendered");
+  }, [text]) 
+  */
+
+  const [posts, setPosts] = useState([]);
+  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("posts/timeline/61a5d5109b0c7c03e147c933");
+      setPosts(res.data)
+    }
+    fetchPosts();
+  }, []) 
+
   return (
     <div className="feed">
+      {/* <input type="text" onChange={e=>setText(e.target.value)} /> */}
       <div className="feedWrapper">
         <Share />
-        {Posts.map((p) => (
+        {posts.map((p) => (
           <Post key = {p.id} post={p} />
         ))}
       </div>
